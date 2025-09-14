b4d6e5626cde14ddd5e45ad0c9c5d137e830cd40
Douglas <dengratulis@gmail.com>
2025-09-14 14:42:22 -0300

    first commit

---
+++ src/main/java/com/devsuperior/Sistema/Evento/SistemaEventoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Sistema.Evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/Sistema/Evento/SistemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Sistema.Evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
