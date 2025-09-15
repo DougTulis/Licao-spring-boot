8304110e6fefc92961f0d1d24bdf2f648651e694
Douglas <dengratulis@gmail.com>
2025-09-14 21:42:03 -0300

    Lição feita!

--- src/main/java/com/devsuperior/Sistema/Evento/SistemaEventoApplication.java
+++ src/main/java/com/devsuperior/Sistema/Evento/SistemaEventoApplication.java
@@ -9,7 +9,7 @@ public class SistemaEventoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaEventoApplication.class, args);
 		
-		System.out.println("Conexao procedente");
+		
 	}
 
 }
